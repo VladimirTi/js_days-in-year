acd6220edb79ff561c7012c8077169ee91b991e0
VladimirTi <tytmes@mail.ru>
2019-06-04 00:49:24 +0300

    throw new Error added

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 function daysInYear (year) {
-  const promise = new Promise ((resolve, reject) => {
-    typeof(year) === 'number' && Number.isInteger(year) ? resolve(year) : reject('exception');
+  const promise = new Promise((resolve, reject) => {
+    if (typeof(year) === 'number' && Number.isInteger(year)){
+      resolve(year)
+    } else {
+      throw new Error('exception')
+    };
   });
 
   promise
-  .then(
-    year => { 
-      year % 400 === 0 || !(year % 100 === 0 || !(year % 4 === 0)) ? console.log(366) : console.log(365);
-    }
-  )
-  .catch(error => console.log(error));
+    .then(
+      year => { 
+        year % 400 === 0 || !(year % 100 === 0 || !(year % 4 === 0)) ? console.log(`in ${year} year 366 days`) : console.log(`in ${year} year 365 days`);
+      }
+    )
+    .catch(error => console.log(error));
 }
 
 daysInYear('2019'); // exception
