99058f76e6ab15dcc6232c22ae9ec4b08450492c
VladimirTi <tytmes@mail.ru>
2019-06-10 00:03:51 +0300

    throw new Error

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
 function daysInYear (year) {
-  const promise = new Promise((resolve, reject) => {
-    if (typeof(year) === 'number' && Number.isInteger(year)){
-      resolve(year)
+    if (Number.isInteger(year)){
+      return year % 400 === 0 || !(year % 100 === 0 || !(year % 4 === 0)) ? 366 : 365;
     } else {
-      reject(new Error('exception'))
-    };
-  });
+     throw new Error('exception');
+    }
+  }
 
-  promise
-    .then(
-      year => { 
-        year % 400 === 0 || !(year % 100 === 0 || !(year % 4 === 0)) ? console.log(`in ${year} year 366 days`) : console.log(`in ${year} year 365 days`);
-      }
-    )
-    .catch(error => console.log(error));
-}
 
-daysInYear('2019'); // exception
-daysInYear({year: 2100}); // exception
-daysInYear(2020.2); // exception
-daysInYear(2019); // 365
-daysInYear(2020); // 366
-daysInYear(2100); // 365
-daysInYear(2000); // 366
+console.log(daysInYear('2019')); // exception
+console.log(daysInYear({year: 2100})); // exception
+console.log(daysInYear(2020.2)); // exception
+console.log(daysInYear(2019)); // 365
+console.log(daysInYear(2020)); // 366
+console.log(daysInYear(2100)); // 365
+console.log(daysInYear(2000)); // 366
